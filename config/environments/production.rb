62badbfc1df298aa4f793cf4baff0b105410eeea
rlattorra <rlattorra@gmail.com>
2025-04-18 03:41:57 +0000

    chore: Edit hosts config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,10 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
+  config.hosts = [
+  "effective-orbit-7v5q6gxgg9qx2pjr5-3000.app.github.dev",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+   ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
